7124d106f78729d8269d223addb7ad2ed4bafe63
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-01 17:27:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def index():
 @app.route('/results/<data>')
 def results(data):
     text_to_classify = data
-    pred = predict(text_to_classify)
-    return render_template('results.html', pred=pred, text_to_classify=text_to_classify)
+    pred , scores= predict(text_to_classify)
+    return render_template('results.html', pred=pred,scores=scores, text_to_classify=text_to_classify)
 
 
 if __name__ == '__main__':
