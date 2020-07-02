6d607cd4f1d0a759192cac162e1609f3ea4b1536
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-02 17:50:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,19 +15,19 @@ def index():
 @app.route('/results/<data>')
 def results(data):
     text_to_classify = data
-    pred,scores,classes = predict(text_to_classify)
+    pred = predict(text_to_classify)
     
 
 #     x = ['Nuclear', 'Hydro', 'Gas', 'Oil', 'Coal', 'Biofuel']
 #     energy = [5, 6, 15, 22, 24, 8]
     
 
-    x_pos = [i for i, _ in enumerate(classes)]
-    fig, ax = plt.subplots()
-    plt.bar(x_pos, scores, color='red')
-    plt.xlabel("categories")
-    plt.ylabel("Toxicity")
-    plt.title("Toxicity measurements")
+#     x_pos = [i for i, _ in enumerate(classes)]
+#     fig, ax = plt.subplots()
+#     plt.bar(x_pos, scores, color='red')
+#     plt.xlabel("categories")
+#     plt.ylabel("Toxicity")
+#     plt.title("Toxicity measurements")
 #     fig.savefig('my_plot.png')
     return render_template('results.html', pred=pred, text_to_classify=text_to_classify)
 
