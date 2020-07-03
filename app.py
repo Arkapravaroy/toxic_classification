45386cd81dd0713494ecc3a0a17be122e9613597
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-03 16:39:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ def results(data):
     pred,scores,classes = predict(text_to_classify)
     fig = plt.figure()
     ax = fig.add_axes([0,0,1,1])
-    langs = ['C', 'C++', 'Java', 'Python', 'PHP']
-    students = [23,17,35,29,12]
-    ax.bar(langs,students)
+#     langs = ['C', 'C++', 'Java', 'Python', 'PHP']
+#     students = [23,17,35,29,12]
+    ax.bar(classes,pred)
     
 #     plt.show()
 
