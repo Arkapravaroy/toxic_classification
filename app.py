0dc42937b691463f0c2c7835a5681428bf9123ff
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-03 11:25:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,12 @@ def index():
 def results(data):
     text_to_classify = data
     pred,scores,classes = predict(text_to_classify)
-    return render_template('results.html', pred=pred, text_to_classify=text_to_classify)
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(classes,scores)
+    plt.show()
+    image=fig.savefig()
+    return render_template('results.html', pred=pred, text_to_classify=text_to_classify, user_image=image)
 
 
 if __name__ == '__main__':
