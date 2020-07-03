c3b1de6833f58d127d94adb427b1b10643a5da3d
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-03 12:34:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,15 +16,7 @@ def index():
 def results(data):
     text_to_classify = data
     pred,scores,classes = predict(text_to_classify)
-    fig = plt.figure()
-    ax = fig.add_axes([0,0,1,1])
-#     langs = ['C', 'C++', 'Java', 'Python', 'PHP']
-#     students = [23,17,35,29,12]
-    ax.bar(classes,scores)
-    # plt.show()
-    os.remove("templates\image2.png")
-    #Now save the new image file
-    fig.savefig("templates\image2.png")
+
 #     fig.savefig('image2.png')
     
     return render_template('results.html', pred=pred, text_to_classify=text_to_classify)
