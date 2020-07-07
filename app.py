849460e1cb8cee7893d632a6522f6cc560ce2c45
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-07 14:27:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def results(data):
     ax = fig.add_axes([0,0,1,1])
 #     langs = ['C', 'C++', 'Java', 'Python', 'PHP']
 #     students = [23,17,35,29,12]
-    ax.bar(classes,scores)
+    ax.bar(classes,scores,color=(0.5,0.5,0.5,1))
 #     ax.set_facecolor('black')
     ax.patch.set_facecolor('white')
     ax.tick_params(axis='x', colors='black')
