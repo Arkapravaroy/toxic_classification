7e4d595a74211f4631787c03d1ec19ec736c1bfc
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-13 19:47:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ def results(data):
     text_to_classify = data
     pred,scores,classes = predict(text_to_classify)
     fig = plt.figure()
+    fig.ylim(0.1)
     ax = fig.add_axes([0,0,1,1])
 #     langs = ['C', 'C++', 'Java', 'Python', 'PHP']
 #     students = [23,17,35,29,12]
     ax.bar(classes,scores,color=(0.5,0.5,0.5,1))
 #     ax.set_facecolor('black')
 #     ax.patch.set_facecolor('white')
-    ax.ylim(0.1)
+    
     ax.tick_params(axis='x', colors='black')
     ax.tick_params(axis='y', colors='black')
     img_name="image"+ str(time.time())+".png"
