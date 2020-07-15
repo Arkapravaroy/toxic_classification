2ffeaaaaa3510a5d3ec71094e278b5a37d3c4697
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-15 19:44:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def results(data):
     ax.bar(classes,scores,color=(0.5,0.5,0.5,1))
     ax.set_ylim([0,1])
     img_name="image"+ str(time.time())+".png"
-    fig.savefig('static/' +img_name,bbox_inches='tight',pad_inches=0.5)
+    fig.savefig(img_name,bbox_inches='tight',pad_inches=0.5)
     return render_template('results.html', pred=pred)
 
 
