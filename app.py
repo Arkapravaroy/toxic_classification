3fa9eb6fe294a6f5e53b346a1ef4095efe7ac9cf
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-16 10:47:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -24,8 +24,9 @@ def results(data):
     ax.bar(list_classes,scores,color=(0.5,0.5,0.5,1))
     ax.set_ylim([0,1])
     img_name="image"+ str(time.time())+".png"
+    file_name="file"+str(time.time())+".txt"
     fig.savefig('static/'+img_name,bbox_inches='tight',pad_inches=0.5)
-    with open("static/"+"file"+str(time.time())+".txt", "w") as output:
+    with open("static/"+file_name, "w") as output:
         output.write(str(pred))
     return render_template('results.html', pred=pred, text_to_classify=text_to_classify,graph=img_name)
     
