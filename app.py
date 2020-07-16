d20523e89cde5477549da9cb72bd8fed506dda01
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-07-16 10:42:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,38 +16,19 @@ def index():
 @app.route('/results/<data>')
 def results(data):
     
-#     try:
-#         text_to_classify = data
-#         pred,scores,list_classes = predict(text_to_classify)
-#         fig = plt.figure()
-#         ax = fig.add_axes([0,0,1,1])
-#         ax.bar(list_classes,scores,color=(0.5,0.5,0.5,1))
-#         ax.set_ylim([0,1])
-#         img_name="image"+ str(time.time())+".png"
-#         fig.savefig('static/'+img_name,bbox_inches='tight',pad_inches=0.5)
-#     except:
-#         text_to_classify=''
-#         pred=['error','error','error','error','error','error']
-#         list_classes=['toxic', 'severe_toxic', 'obscene', 'threat', 'insult', 'identity_hate']
-#         fig = plt.figure()
-#         ax = fig.add_axes([0,0,1,1])
-# #         ax.bar(list_classes,scores,color=(0.5,0.5,0.5,1))
-#         ax.set_ylim([0,1])
-#         img_name="image"+ str(time.time())+".png"
-#         fig.savefig('static/'+img_name,bbox_inches='tight',pad_inches=0.5)
-#     return render_template('results.html', pred=pred, text_to_classify=text_to_classify,graph=img_name)
-    if data != '':
-        text_to_classify = data
-        pred,scores,list_classes = predict(text_to_classify)
-        fig = plt.figure()
-        ax = fig.add_axes([0,0,1,1])
-        ax.bar(list_classes,scores,color=(0.5,0.5,0.5,1))
-        ax.set_ylim([0,1])
-        img_name="image"+ str(time.time())+".png"
-        fig.savefig('static/'+img_name,bbox_inches='tight',pad_inches=0.5)
-        return render_template('results.html', pred=pred, text_to_classify=text_to_classify,graph=img_name)        
-    else:
-        return render_template('results.html')
+
+    text_to_classify = data
+    pred,scores,list_classes = predict(text_to_classify)
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(list_classes,scores,color=(0.5,0.5,0.5,1))
+    ax.set_ylim([0,1])
+    img_name="image"+ str(time.time())+".png"
+    fig.savefig('static/'+img_name,bbox_inches='tight',pad_inches=0.5)
+    with open("static/"+"file"+str(time.time())+".txt", "w") as output:
+        output.write(str(pred))
+    return render_template('results.html', pred=pred, text_to_classify=text_to_classify,graph=img_name)
+    
 
 if __name__ == '__main__':
     app.run()
